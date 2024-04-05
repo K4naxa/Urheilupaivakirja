d3427ee0f6cc2fb54a4ef057fa39abe91d6a0611
K4naxa <k4na@iki.fi>
2024-04-05 20:28:26 +0300

    finished login site

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -6,6 +6,7 @@ import { MainContext } from "./mainContext";
 import LoginPage from "./pages/Login/LoginPage";
 import StudentHome from "./pages/StudentHome/StudentHome";
 import TeacherHome from "./pages/TeacherHome/TeacherHome";
+import Register from "./pages/Register/Register";
 
 function App() {
   // get loggedIn from MainContext
@@ -21,11 +22,15 @@ function App() {
 
   return (
     <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/resetPassword" element={<div>Reset Password</div>} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/studentHome" element={<StudentHome />} />
       <Route path="/" element={<TeacherHome />} />
     </Routes>
   );
 }
+
+// TODO 1: Add a new route to the App component that renders a component called ResetPassword.
 
 export default App;
