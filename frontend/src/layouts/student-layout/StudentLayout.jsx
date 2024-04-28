fe32ad14c6a3b9a416f783c703babe71243bb7a0
Jami H. <K4na@iki.fi>
2024-04-28 21:24:04 +0300

    Update logout button text in StudentLayout.jsx

--- frontend/src/layouts/student-layout/StudentLayout.jsx
+++ frontend/src/layouts/student-layout/StudentLayout.jsx
@@ -103,7 +103,7 @@ const StudentLayout = () => {
                 }}
               >
                 <FiLogOut />
-                <p className="text-[12px]">Asetukset</p>
+                <p className="text-[12px]">Logout</p>
               </button>
             </div>
           </div>
