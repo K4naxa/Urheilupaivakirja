95cf2e842bfaa0ed5f7eb29956af95ad5f66b4ca
Jami H. <K4na@iki.fi>
2024-05-10 08:23:28 +0300

    buttons to change showdate

--- frontend/src/pages/testisivu.jsx
+++ frontend/src/pages/testisivu.jsx
@@ -1,4 +1,8 @@
 import { useMemo } from "react";
+import { FiChevronLeft } from "react-icons/fi";
+import { FiChevronRight } from "react-icons/fi";
+import { IconContext } from "react-icons/lib";
+
 import HeatMap_Month from "../components/HeatMap_Month";
 import trainingService from "../services/trainingService";
 import formatDate from "../utils/formatDate";
@@ -17,22 +21,58 @@ import { useQuery } from "@tanstack/react-query";
 
 import { useMainContext } from "../hooks/mainContext";
 
-function CalendarDay({ day, showWeekName, showDate }) {
-  return (
-    <div
-      className={cc(
-        "MonthDate relative",
-        !isSameMonth(day, showDate) && "bg-bgPrimary ",
-        isToday(day) && "border  border-headerPrimary"
-      )}
-    >
-      {showWeekName && (
-        <div className="absolute -top-6 left-1/2 transform -translate-x-1/2 text-textSecondary text-xs">
-          {formatDate(day, { weekday: "short" })}
-        </div>
-      )}
+function Testisivu() {
+  const { showDate, setShowDate } = useMainContext();
+  const { data: studentJournalData } = useQuery({
+    queryKey: ["studentJournal"],
+    queryFn: () => trainingService.getAllUserJournalEntries(),
+    staleTime: 15 * 60 * 1000, // = 15 minutes in milliseconds - how long to use the cached data before re-fetching
+  });
 
-      {isSameMonth(day, showDate) && formatDate(day, { day: "numeric" })}
+  return (
+    <div className="flex flex-col max-w-96 w-full gap-1">
+      <div className="flex w-full flex-col text-center">
+        <h2 className="text-textSecondary">{showDate.getFullYear()}</h2>
+        <div className="hover: flex justify-center gap-4">
+          <button
+            className="hover:underline"
+            onClick={() => {
+              // Subtract one month from the current showDate
+              const previousMonth = new Date(showDate);
+              previousMonth.setMonth(previousMonth.getMonth() - 1);
+              setShowDate(previousMonth); // Update showDate
+            }}
+          >
+            <IconContext.Provider
+              value={{ className: "hover:text-graphPrimary" }}
+            >
+              <FiChevronLeft />
+            </IconContext.Provider>
+          </button>
+          <p className="text-xl w-24">
+            {formatDate(showDate, { month: "long" })}
+          </p>
+          <button
+            className="hover:fill-blue-500 hover:underline"
+            onClick={() => {
+              // Subtract one month from the current showDate
+              const previousMonth = new Date(showDate);
+              previousMonth.setMonth(previousMonth.getMonth() + 1);
+              setShowDate(previousMonth); // Update showDate
+            }}
+          >
+            <IconContext.Provider
+              value={{ className: "hover:text-graphPrimary" }}
+            >
+              <FiChevronRight />
+            </IconContext.Provider>
+          </button>
+        </div>
+      </div>
+      <div>
+        <MonthCalendar journal={studentJournalData} />
+        {/* <HeatMap_Month journal={studentJournalData} /> */}
+      </div>
     </div>
   );
 }
@@ -65,17 +105,22 @@ function MonthCalendar({ journal }) {
   );
 }
 
-function Testisivu() {
-  const { data: studentJournalData } = useQuery({
-    queryKey: ["studentJournal"],
-    queryFn: () => trainingService.getAllUserJournalEntries(),
-    staleTime: 15 * 60 * 1000, // = 15 minutes in milliseconds - how long to use the cached data before re-fetching
-  });
-
+function CalendarDay({ day, showWeekName, showDate }) {
   return (
-    <div className="flex flex-col max-w-96 w-full gap-24">
-      <MonthCalendar journal={studentJournalData} />
-      {/* <HeatMap_Month journal={studentJournalData} /> */}
+    <div
+      className={cc(
+        "MonthDate relative",
+        !isSameMonth(day, showDate) && "bg-bgPrimary ",
+        isToday(day) && "border  border-headerPrimary"
+      )}
+    >
+      {showWeekName && (
+        <div className="absolute -top-6 left-1/2 transform -translate-x-1/2 text-textSecondary text-xs">
+          {formatDate(day, { weekday: "short" })}
+        </div>
+      )}
+
+      {isSameMonth(day, showDate) && formatDate(day, { day: "numeric" })}
     </div>
   );
 }
