f82f3284d985921191bb86aab757fa3b074c0e78
Jami H. <K4na@iki.fi>
2024-05-27 20:31:28 +0300

    start of verifoi window

--- backend/routes/user/unverifiedRouter.js
+++ backend/routes/user/unverifiedRouter.js
@@ -15,21 +15,75 @@ const getTokenFrom = (request) => {
     : null;
 };
 
-// checks if request was done by admin / role 1 > returns all unverified users
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   const role = getRole(req);
+
   if (role !== 1) {
     return res.status(401).json({ error: "Unauthorized" });
-  } else {
-    const unverifiedUsers = knex("users")
-      .where("email_verified", "=", 0)
-      .then((rows) => {
-        if (rows.length === 0) {
-          return res.status(404).json({ error: "No unverified users found" });
-        } else {
-          res.json(rows);
-        }
-      });
+  }
+
+  const getUnverifiedStudents = () => {
+    return knex("students as s")
+      .select(
+        "s.first_name",
+        "s.last_name",
+        "s.user_id",
+        "sg.group_identifier as group",
+        "c.name as campus",
+        "sp.name as sport",
+        "u.email"
+      )
+      .where("s.verified", false)
+      .leftJoin("users as u", "s.user_id", "u.id")
+      .leftJoin("student_groups as sg", "s.group_id", "sg.id")
+      .leftJoin("campuses as c", "s.campus_id", "c.id")
+      .leftJoin("sports as sp", "s.sport_id", "sp.id")
+      .groupBy(
+        "s.user_id",
+        "u.email",
+        "s.first_name",
+        "s.last_name",
+        "s.group_id",
+        "s.campus_id",
+        "s.sport_id"
+      );
+  };
+
+  // Future expansions
+  const getUnverifiedSports = () => {
+    return knex("sports")
+      .select("sports.id", "sports.name")
+      .where("verified", false);
+  };
+
+  const getUnverifiedCampuses = () => {
+    return knex("campuses")
+      .select("campuses.id", "campuses.name")
+      .where("verified", false);
+  };
+
+  try {
+    const [
+      students,
+      // sports,
+      // campuses,
+    ] = await Promise.all([
+      getUnverifiedStudents(),
+      // getUnverifiedSports(),
+      // getUnverifiedCampuses(),
+    ]);
+
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({
+      students,
+      // sports,
+      // campuses,
+    });
+  } catch (err) {
+    console.error("Error fetching unverified data", err);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching unverified data" });
   }
 });
 
--- frontend/src/pages/teacher/verify/Verify.jsx
+++ frontend/src/pages/teacher/verify/Verify.jsx
@@ -1,51 +1,108 @@
 import userService from "../../../services/userService";
-import { useContext, useEffect, useState } from "react";
-
-const createUserContainer = (user, setUsers) => {
-  const handleVerify = () => {
-    userService.verifyUser(user).then(() => {
-      setUsers((prevUsers) =>
-        prevUsers.filter((prevUser) => prevUser.id !== user.id)
-      );
-    });
-  };
-  const handleDelete = () => {
-    userService.deleteUser(user.id).then(() => {
-      setUsers((prevUsers) =>
-        prevUsers.filter((prevUser) => prevUser.id !== user.id)
-      );
-    });
-  };
-
-  return (
-    <div key={user.id} className="userContainer">
-      <div>{user.email}</div>
-      <button onClick={() => handleVerify()}>Verify</button>
-      <button onClick={() => handleDelete()}>Delete</button>
-    </div>
-  );
-};
+import { FiTrash2, FiCheck } from "react-icons/fi";
+import { useEffect, useState } from "react";
 
 const Verify = () => {
-  const [unverifiedUsers, setUnverifiedUsers] = useState([]);
+  const [unverifiedStudents, setUnverifiedStudents] = useState([]);
 
   useEffect(() => {
-    userService.getUnverifiedUsers().then((users) => {
-      setUnverifiedUsers(users);
+    userService.getAllUnverified().then((users) => {
+      console.log(users);
+      setUnverifiedStudents(users?.students);
     });
   }, []);
 
   return (
-    <div>
-      <h1>Verify</h1>
-      <div className="unverifiedUserListContainer">
-        {unverifiedUsers.map((user) =>
-          createUserContainer(user, setUnverifiedUsers)
-        )}
+    <div className="flex w-full justify-center flex-wrap mt-8 ">
+      {/* Student container */}
+      <div
+        className="flex flex-col bg-bgSecondary rounded-md border border-borderPrimary
+      min-w-96 "
+      >
+        <div
+          className="w-full py-2 rounded-t-md 
+         text-xl text-center"
+        >
+          <h2>Oppilaat</h2>
+        </div>
+        <div className=" w-full py-2 rounded-b-md border border-borderPrimary">
+          <div className=" flex flex-wrap gap-4 p-2 bg-bgSecondary">
+            {unverifiedStudents.map((student) => (
+              <CreateStudentContainer
+                key={student.user_id}
+                student={student}
+                setUnverifiedStudents={setUnverifiedStudents}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
 };
+
+function CreateStudentContainer({ student, setUnverifiedStudents }) {
+  const handleVerify = () => {
+    console.log(student.user_id);
+    userService.verifyUser(student.user_id).then(() => {
+      setUnverifiedStudents((prevStudents) =>
+        prevStudents.filter((prevStudents) => prevStudents.id !== student.id)
+      );
+    });
+  };
+  const handleDelete = () => {
+    userService.deleteUser(student.user_id).then(() => {
+      setUnverifiedStudents((prevStudents) =>
+        prevStudents.filter((prevStudents) => prevStudents.id !== student.id)
+      );
+    });
+  };
+
+  return (
+    <div
+      key={student.id}
+      className="flex flex-col hover:bg-bgGray p-4 rounded-md gap-2
+      border border-borderPrimary"
+    >
+      {/* User Info */}
+      <div className="text-center text-xl">
+        {student.first_name} {student.last_name}
+      </div>
+      <div className="flex gap-1">
+        <p className="text-textSecondary w-20">S-posti:</p>{" "}
+        <p>{student.email}</p>
+      </div>
+      <div className="flex gap-1">
+        <p className="text-textSecondary w-20">Laji:</p> <p>{student.sport}</p>
+      </div>
+
+      <div className="flex gap-1">
+        <p className="text-textSecondary w-20">Ryhmä:</p> <p>{student.group}</p>
+      </div>
+      <div className="flex gap-1">
+        <p className="text-textSecondary w-20">Toimipiste:</p>{" "}
+        <p>{student.campus}</p>
+      </div>
+
+      {/* Control Buttons */}
+      <div className="flex gap-4 justify-center">
+        {" "}
+        <button
+          className="text-iconGreen p-1 rounded-md "
+          onClick={() => handleVerify()}
+        >
+          <FiCheck size={20} />
+        </button>
+        <button
+          className="text-iconRed p-1  rounded-md"
+          onClick={() => handleDelete()}
+        >
+          <FiTrash2 size={20} />
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default Verify;
 // Path: frontend/src/pages/verify/Verify.jsx
