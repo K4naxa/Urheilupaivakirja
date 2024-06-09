e0eec94015f98bc8d0dfec792420f14ffa55545d
K4naxa <K4na@iki.fi>
2024-06-09 18:04:09 +0300

    new icons for the site and fixes to pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,61 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPWAPlugIn)],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      theme_color: "rgb(44, 114, 245)",
+      background_color: "rgb(var(--color-bg-primary))",
+      display: "standalone",
+      scope: "/",
+      start_url: "/",
+      orientation: "portrait",
+
+      manifest: {
+        name: "Urheilupäiväkirja",
+        short_name: "U-päiväkirja",
+        description: "Sivu urheilusuoritusten kirjaamiseen, sekä seurantaan",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        devOptions: {
+          enabled: true,
+        },
+        client: {
+          installPrompt: true,
+        },
+      },
+    }),
+  ],
+  preview: {
+    https: true,
+  },
   server: {
+    server: { https: true }, // Not needed for Vite 5+
     proxy: {
       "/user/login": {
         target: "http://localhost:3001",
@@ -80,13 +133,43 @@ export default defineConfig({
 });
 
 const manifestForPWAPlugIn = {
-  registerType: "prompt",
+  includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+  registerType: "autoUpdate",
   manifest: {
     name: "Urheilupäiväkirja",
     short_name: "U-päiväkirja",
     description: "Sivu urheilusuoritusten kirjaamiseen, sekä seurantaan",
+    icons: [
+      {
+        src: "/frontend/pwa/pwa-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/frontend/pwa/pwa-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "pwa-maskable-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "pwa-maskable-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
     devOptions: {
       enabled: true,
+    },
+    client: {
+      installPrompt: true,
     },
 
     theme_color: "rgb(var(--color-primary))",
