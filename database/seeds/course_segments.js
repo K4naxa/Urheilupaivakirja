1978b83186981936543ff671669b40b84506d0ac
K4naxa <K4na@iki.fi>
2024-08-18 18:40:12 +0300

    refactor: Update course segment names and values in seed file

--- database/seeds/course_segments.js
+++ database/seeds/course_segments.js
@@ -6,8 +6,8 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("course_segments").del();
   await knex("course_segments").insert([
-    { id: 1, name: "first_segment", value: 100, order_number: 1 },
-    { id: 2, name: "second_segment", value: 100, order_number: 2 },
-    { id: 3, name: "third_segment", value: 100, order_number: 3 },
+    { id: 1, name: "Aloittelija", value: 100, order_number: 1 },
+    { id: 2, name: "Harrastelija", value: 100, order_number: 2 },
+    { id: 3, name: "Ammatti urheilija", value: 100, order_number: 3 },
   ]);
 };
