af71da40945602fc7deeda1fc5f5ac9f14a5e814
Juho Vasaramäki <juho.vasaramaki@edu.tampere.fi>
2024-08-22 20:59:47 +0300

    separated development and production seeds. use --env production and --env development when running seeds

---
+++ database/seeds/production/13_workout_categories.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex("workout_categories").del();
+  await knex("workout_categories").insert([
+    { name: "Oma laji" },
+    { name: "Voima" },
+    { name: "Nopeus" },
+    { name: "Kestävyys" },
+    { name: "Lihashuolto" },
+  ]);
+};
