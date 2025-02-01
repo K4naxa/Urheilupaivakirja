4a12ba91552e10e08bf10a78884c08f7aa12357a
Jami H <K4na@iki.fi>
2025-02-01 10:10:50 +0200

    bugfix: disabled submit button for journals when one is already being sent

--- frontend/src/pages/student/journal-entry/NewJournalEntryPage.jsx
+++ frontend/src/pages/student/journal-entry/NewJournalEntryPage.jsx
@@ -30,7 +30,6 @@ const NewJournalEntryPage = ({ onClose, studentData, date }) => {
   });
 
   const studentDataLoading = false;
-
   const [errors, setErrors] = useState({});
   const [showDetails, setShowDetails] = useState(false);
   const [conflict, setConflict] = useState({
@@ -108,8 +107,11 @@ const NewJournalEntryPage = ({ onClose, studentData, date }) => {
 
   const newJournalEntryHandler = async (e) => {
     e.preventDefault();
+    setSubmitButtonIsDisabled(true);
+
     setErrors("");
     if (!errorCheckJournalEntry()) {
+      setSubmitButtonIsDisabled(false);
       return;
     }
 
@@ -121,13 +123,26 @@ const NewJournalEntryPage = ({ onClose, studentData, date }) => {
         onAgree: handleUserConfirmation,
         closeOnOutsideClick: false,
       });
+      setSubmitButtonIsDisabled(false);
       return;
     }
+    console.log("submit button state: ", submitButtonIsDisabled);
 
     try {
-      addJournalEntry.mutate({ newJournalEntryData });
+      await addJournalEntry.mutateAsync({ newJournalEntryData });
+
+      console.log(
+        "submit button state after mutation: ",
+        submitButtonIsDisabled
+      );
+      setSubmitButtonIsDisabled(false);
+      console.log(
+        "submit button state after setSubmitButtonIsDisabled: ",
+        submitButtonIsDisabled
+      );
     } catch (error) {
       console.error("Error adding journal entry:", error);
+      setSubmitButtonIsDisabled(false);
     }
   };
 
@@ -624,15 +639,38 @@ const NewJournalEntryPage = ({ onClose, studentData, date }) => {
             )}
           </div>
 
+          {/* Submit button */}
           <div className="flex flex-col items-center w-full gap-4 p-4 mt-auto text-center text-red-400">
             {conflict.messageShort && <p>{conflict.messageShort}</p>}
             <button
-              className={`min-w-[160px] text-white px-4 py-4 rounded-md bg-primaryColor border-borderPrimary active:scale-95 transition-transform duration-75 hover:bg-hoverPrimary
-    ${submitButtonIsDisabled ? "bg-gray-400 opacity-20 text-gray border-gray-300 cursor-not-allowed" : "cursor-pointer"}`}
+              className={`min-w-[160px] text-white px-4 py-4 rounded-md   active:scale-95 transition-all duration-150
+    ${submitButtonIsDisabled ? "bg-bgPrimary cursor-not-allowed" : "border-borderPrimary cursor-pointer bg-primaryColor"}`}
               type="submit"
               disabled={submitButtonIsDisabled}
             >
-              {getSubmitButtonText(newJournalEntryData.entry_type)}
+              {submitButtonIsDisabled ? (
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    className="w-8 h-8 text-borderPrimary animate-spin  fill-primaryColor mx-auto"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>
+              ) : (
+                getSubmitButtonText(newJournalEntryData.entry_type)
+              )}
             </button>
           </div>
         </form>
